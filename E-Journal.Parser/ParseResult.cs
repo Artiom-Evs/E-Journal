03d8f196cd62bd9c2bf3ce24bf9adcb7904180ab
Artiom-Evs <evs.a.s@mail.ru>
2022-02-09 18:16:40 +0300

    Add ParseResult model
    This model represents the results of parsing of timetable of a particular group or teacher.

---
+++ E-Journal.Parser/ParseResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Journal.Parser
+{
+    public class ParseResult
+    {
+        public string? Name { get; set; }
+        public string? DateRange { get; init; }
+        public DateTime[]? Days { get; init; }
+        public string[][]? Timetable { get; init; }
+        public Exception? Exception { get; init; }
+    }
+}
