fa5038356dfad2082374f64f44bfe6bdba3154a1
Artiom-Evs <evs.a.s@mail.ru>
2022-02-12 16:40:23 +0300

    Add Subgroup property in the TrainingSession class

--- E-Journal.Shared/TrainingSession.cs
+++ E-Journal.Shared/TrainingSession.cs
@@ -9,14 +9,17 @@ namespace E_Journal.Shared
         {
             StudentStatuses = new List<StudentStatus>();
             Room = String.Empty;
+            Subgroup = ' ';
         }
 
         public int Id { get; set; }
         public DateTime Date { get; set; }
+        public byte Number { get; set; }
         public string Room { get; set; }
         public Discipline Discipline { get; set; }
         public Teacher Teacher { get; set; }
         public Group Group { get; set; }
+        public char Subgroup { get; set; }
         public ICollection<StudentStatus> StudentStatuses { get; set; }
 
         public override string ToString()
