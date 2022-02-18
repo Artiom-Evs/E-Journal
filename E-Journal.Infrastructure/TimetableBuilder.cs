ff43aee43119c28a9920cc9b769cbd93a06b22b2
Artiom-Evs <evs.a.s@mail.ru>
2022-02-18 19:00:57 +0300

    Remove E-Journal.Builder library
    E-Journal.Builder library cannot to use without E-Journal.Infrastructure, because ObjectBuilder class needs access to database. And E-Journal.Infrastructure library cannot to use without E-Journal.Builder library, because E-Journal.Shared library models build in ObjectBuilder class.
    E-Journal.Builder library has been removed.
    Functionality of E-Journal.Builder library has been moved to E-Journal.Infrastructure library.
    TimetableBuilder class has been added to E-Journal.Infrastructure. This class has inherited functionality of ObjectBuilder class.

---
+++ E-Journal.Infrastructure/TimetableBuilder.cs
@@ -0,0 +1,138 @@
+﻿using E_Journal.Shared;
+using E_Journal.Parser;
+
+namespace E_Journal.Infrastructure
+{
+    public class TimetableBuilder
+    {
+        private readonly IJournalRepository repository;
+
+        public TimetableBuilder(IJournalRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async IAsyncEnumerable<Group> BuildAllGroups(ParseResult[] results)
+        {
+            foreach (var result in results)
+            {
+                yield return await BuildGroup(result);
+            }
+        }
+        public async ValueTask<Group> BuildGroup(ParseResult result)
+        {
+            Group group = await GetOrCreateGroup(result.Name);
+            Timetable timetable = new() { Group = group };
+            group.Timetables.Add(timetable);
+
+            if (result.DateRange is null || result.Days is null || result.Timetable is null)
+            {
+                return group;
+            }
+
+            DateTime[] dates = result.DateRange.Split(" - ").Select(d => DateTime.Parse(d)).ToArray();
+            timetable.StartDate = dates[0];
+            timetable.EndDate = dates[1];
+
+            foreach (var (First, Second) in result.Days.Zip(result.Timetable))
+            {
+                await foreach (var session in BuildDay(First, Second))
+                {
+                    session.Group = group;
+                    Add<Discipline>(group.Disciplines, session.Discipline);
+                    Add<Group>(session.Discipline.Groups, group);
+                    timetable.TrainingSessions.Add(session);
+                }
+            }
+
+            return group;
+        }
+        private async IAsyncEnumerable<TrainingSession> BuildDay(DateTime date, string[] day_sessions)
+        {
+            for (int i = 0; i < day_sessions.Length; i += 2)
+            {
+                if (day_sessions[i] == "") continue;
+
+                await foreach (var session in BuildCell(day_sessions[i], day_sessions[i + 1], date, i / 2 + 1))
+                {
+                    yield return session;
+                }
+            }
+        }
+        private async IAsyncEnumerable<TrainingSession> BuildCell(string cell, string room, DateTime date, int number)
+        {
+            var distRows = cell.Replace("1.", "").Replace("2.", "").Replace("3.", "").Split('\n');
+            var roomRows = room.Replace("1.", "").Replace("2.", "").Replace("3.", "").Split('\n');
+
+            for (int row = 0; row < distRows.Length; row += 3)
+            {
+                var discipline = await GetOrCreateDiscipline(distRows[row]);
+                var teacher = await GetOrCreateTeacher(distRows[row + 2]);
+
+                Add<Teacher>(discipline.Teachers, teacher);
+                Add<Discipline>(teacher.Disciplines, discipline);
+
+                TrainingSession session = new()
+                {
+                    Date = date,
+                    Discipline = discipline,
+                    Teacher = teacher,
+                    Room = (row / 3 < roomRows.Length) ? roomRows[row / 3] : roomRows[0],
+                    Number = (byte)number
+                };
+
+                if (distRows.Length > 3)
+                {
+                    session.Subgroup = char.Parse((row / 3 + 1).ToString());
+                }
+
+                discipline.TrainingSessions.Add(session);
+                yield return session;
+            }
+        }
+
+        private async ValueTask<Group> GetOrCreateGroup(string group_name)
+        {
+            Group? group = repository.Groups.FirstOrDefault(g => g.Name == group_name);
+
+            if (group == null)
+            {
+                group = new() { Name = group_name };
+                await repository.Add<Group>(group);
+            }
+
+            return group;
+        }
+        private async ValueTask<Discipline> GetOrCreateDiscipline(string discipline_name)
+        {
+            Discipline? discipline = repository.Disciplines.FirstOrDefault(d => d.Name == discipline_name);
+
+            if (discipline == null)
+            {
+                discipline = new() { Name = discipline_name };
+                await repository.Add<Discipline>(discipline);
+            }
+
+            return discipline;
+        }
+        private async ValueTask<Teacher> GetOrCreateTeacher(string teacher_name)
+        {
+            Teacher? teacher = repository.Teachers.FirstOrDefault(t => t.Name == teacher_name);
+
+            if (teacher == null)
+            {
+                teacher = new() { Name = teacher_name };
+                await repository.Add<Teacher>(teacher);
+            }
+
+            return teacher;
+        }
+        private static void Add<T>(ICollection<T> list, T item)
+        {
+            if (!list.Contains(item))
+            {
+                list.Add(item);
+            }
+        }
+    }
+}
