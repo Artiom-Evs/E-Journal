8beb1e79db54548520d42db5195586f047f288ef
Artiom-Evs <evs.a.s@mail.ru>
2022-03-01 16:34:22 +0300

    Remove unnecessary E-Journal.Server components
    Unnecessary components of project sample have been removed.
    Startup class has also been added.

---
+++ E-Journal.Server/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace E_Journal.Server
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
