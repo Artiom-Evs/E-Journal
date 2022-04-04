b5adc9c71f6ccb742ef4f92fe41e2796e475019d
Artiom-Evs <evs.a.s@mail.ru>
2022-04-04 20:44:44 +0300

    Add new web UI project
    E-Journal.WebUI web UI project has been added because auto-generated Identity code only well works with MVC projects. This project will replace old E-Journal.Server web UI project which based on Blazor Pages.

---
+++ E-Journal.WebUI/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace E_Journal.WebUI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
