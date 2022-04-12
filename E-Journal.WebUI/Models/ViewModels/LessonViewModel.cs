57fbdc518ca30d97fe5b71de83dcbf9de0e6a0f3
Artiom-Evs <evs.a.s@mail.ru>
2022-04-12 23:26:25 +0300

    Add GroupScheduleViewComponent
    ScheduleViewModel and LessonViewModel have also been added.

---
+++ E-Journal.WebUI/Models/ViewModels/LessonViewModel.cs
@@ -0,0 +1,14 @@
+﻿#nullable disable
+
+namespace E_Journal.WebUI.Models.ViewModels
+{
+    public record LessonViewModel
+    {
+        public string TeacherName { get; init; }
+        public string DisciplineName { get; init; }
+        public string Room { get; init; }
+        public DateTime Date { get; init; }
+        public int Number { get; init; }
+        public char Subgroup { get; init; }
+    }
+}
