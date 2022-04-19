a451425ffe8cdca695f8b5de0ba1ea71f0860fee
Artiom-Evs <evs.a.s@mail.ru>
2022-04-19 21:15:42 +0300

    Add ApplicationIdentityDbContext class
    ApplicationUser class has been added.

---
+++ E-Journal.WebUI/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace E_Journal.WebUI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+        }
+
+        public ApplicationUser(string userName) : base(userName)
+        {
+        }
+
+        public int AssociatedId { get; set; }
+        public bool UserConfirmed { get; set; }
+    }
+}
