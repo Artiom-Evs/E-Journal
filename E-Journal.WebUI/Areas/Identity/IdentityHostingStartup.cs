974e55989890600ed8cf746c5f6b8198d19b066c
Artiom-Evs <evs.a.s@mail.ru>
2022-04-24 16:37:17 +0300

    Add default identity UI
    Default identity UI has been created using dotnet codegeneration tools.

---
+++ E-Journal.WebUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using E_Journal.WebUI.Data;
+using E_Journal.WebUI.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(E_Journal.WebUI.Areas.Identity.IdentityHostingStartup))]
+namespace E_Journal.WebUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
