4245c20bf981a592d111e8797e40434962b6aa4c
Artiom-Evs <evs.a.s@mail.ru>
2022-04-25 23:34:14 +0300

    Add LessonCell view component
    GroupSchedule view component has been edited for using new LessonCell view component.

---
+++ E-Journal.WebUI/Components/LessonCellViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using E_Journal.WebUI.Models.ViewModels;
+
+namespace E_Journal.WebUI.Components
+{
+    public class LessonCellViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IEnumerable<LessonViewModel> viewModels)
+        {
+            return View(viewModels.Any() ? viewModels.ToArray() : Array.Empty<LessonViewModel>());
+        }
+    }
+}
