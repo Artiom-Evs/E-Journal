c6fa0f12acca5ed826ad755b286c4494d56e0309
Artiom-Evs <evs.a.s@mail.ru>
2022-05-08 13:33:42 +0300

    Add Score class
    This data model is designed to represent single student score.

---
+++ E-Journal.Shared/Score.cs
@@ -0,0 +1,26 @@
+﻿namespace E_Journal.Shared;
+
+public class Score
+{
+    public int Id { get; set; }
+    public int StudentId { get; set; }
+    public int LessonId { get; set; }
+
+    public Student Student { get; set; }
+    public Lesson Lesson { get; set; }
+
+    public int Value { get; set; }
+
+    public Score() { }
+    public Score(Student student, Lesson lesson, int value)
+    {
+        Student = student;
+        Lesson = lesson;
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return this.Value.ToString();
+    }
+}
