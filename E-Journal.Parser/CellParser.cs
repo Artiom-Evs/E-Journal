6ced9a7eed94782c1cb88f6b786e01afb71bbde7
Artiom-Evs <evs.a.s@mail.ru>
2022-10-16 21:34:43 +0300

    Edit E_journal.Parser library
    Old Schedule parser class has been splitted to PageParser, TableParser and CellParser static classes. They were been optimized.

---
+++ E-Journal.Parser/CellParser.cs
@@ -0,0 +1,189 @@
+﻿using HtmlAgilityPack;
+using E_Journal.Parser.Models;
+using System.Text.RegularExpressions;
+
+namespace E_Journal.Parser;
+
+/// <summary>
+/// Contains methods for parsing schedule table cells
+/// </summary>
+public static class CellParser
+{
+    /// <summary>
+    /// Parse enumerable of schedule cells and return lessons from them
+    /// </summary>
+    public static IEnumerable<Lesson> ParseCells(IEnumerable<PreparsedCell> preparsedCells)
+    {
+        foreach (var cell in preparsedCells)
+        {
+            foreach (var lesson in ParseCell(cell))
+            {
+                yield return lesson;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Parse single schedule cell and return lessons from it
+    /// </summary>
+    public static IEnumerable<Lesson> ParseCell(PreparsedCell cell)
+    {
+        string[] lessonsCellRows = SplitCell(cell.LessonCell).ToArray();
+        string[] roomsCellRows = SplitCell(cell.RoomCell).ToArray();
+
+        // return empty enumerable if cell is empty
+        if (lessonsCellRows.Length == 0)
+        {
+            yield break;
+        }
+
+        // check subgroup numeration
+        // if false, build single lesson
+        // if true, split by subgroup numbers and build lessons from each group of strings
+        if (lessonsCellRows[0][1] != '.' ||
+            !int.TryParse(lessonsCellRows[0][0..1], out _))
+        {
+            yield return BuildLesson(lessonsCellRows, roomsCellRows[0], cell.LessonNumber);
+        }
+        else
+        {
+            List<List<string>> lessons = new();
+            List<string> lesson = new();
+
+            // split all rows by subgroup numbers
+            foreach (var rows in lessonsCellRows)
+            {
+                if (rows[1] == '.' &&
+                    int.TryParse(rows[0..1], out _))
+                {
+                    lesson = new List<string>();
+                    lessons.Add(lesson);
+                }
+
+                lesson.Add(rows);
+            }
+
+            foreach (var (lessonRows, room) in lessons.Zip(roomsCellRows))
+            {
+                yield return BuildLesson(lessonRows.ToArray(), room, cell.LessonNumber);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Split string from lesson cell into strings from lesson cell
+    /// </summary>
+    /// <param name="cell">One cell of the schedule table</param>
+    public static IEnumerable<string> SplitCell(HtmlNode cell)
+    {
+        // check numbered list
+        // it can contain one or more elements, or one shifted element 
+        // if true, table cell converted to the standard string format
+        if (cell.Element("ol") != null)
+        {
+            // take ordered list
+            var ol = cell.Element("ol");
+            // take start value of the ordered list
+            int startNumber = int.Parse(ol.Attributes["start"]?.Value ?? "1");
+
+            // set subgroup numbers for each subgroup lesson and split them
+            var strs = ol
+                .Elements("li")
+                .Select(li => li.InnerHtml.Replace("<p>", "").Replace("</p>", "").Replace("<br>", "\r\n").Trim())
+                .Select(t => $"{startNumber++}.{t}")
+                .Select(t => t.Split("\r\n", StringSplitOptions.TrimEntries));
+
+            foreach (var str in strs)
+            {
+                foreach (var text in str)
+                {
+                    if (text.Equals("&nbsp;") || string.IsNullOrEmpty(text)) continue;
+                    yield return text;
+                }
+            }
+        }
+        else
+        {
+            // split strings from lesson cell
+            var result = cell.Element("p")
+                .Elements("#text")
+                .Select(t => t.InnerText.Trim())
+                .Where(t => !t.Equals("&nbsp;"));
+
+            foreach (var str in result)
+            {
+                yield return str;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Build lesson object based on data from lesson cell
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="NotImplementedException"></exception>
+    public static Lesson BuildLesson(string[] rows, string room, int lessonNumber)
+    {
+        if (rows.Length <= 0)
+        {
+            throw new ArgumentException("Count of rows cannot be less or equal than zero.", nameof(rows));
+        }
+
+        if (string.IsNullOrEmpty(room))
+        {
+            throw new ArgumentException("String cannot be empty.", nameof(room));
+        }
+
+        if (lessonNumber <= 0)
+        {
+            throw new ArgumentException("Value cannot be less or equal than zero.", nameof(lessonNumber));
+        }
+
+        int subgroup = 0;
+
+        // handle subgroup number
+        if (rows[0][1] == '.' ||
+            int.TryParse(rows[0][0..1], out subgroup))
+        {
+            rows[0] = rows[0][2..];
+        }
+
+        // standard lessons
+        if (rows.Length >= 3)
+        {
+            return new Lesson()
+            {
+                Title = rows[0],
+                Type = rows[1].Replace("(", "").Replace(")", ""),
+                Subtitle = rows[2],
+                Room = room,
+                Number = lessonNumber,
+                Subgroup = subgroup == 0 ? null : subgroup
+            };
+        }
+
+        // this block designed to handle lessons that do not have separator before lesson type
+        // such lessons consists to two lines
+        else if (rows.Length == 2)
+        {
+            Regex reg = new(@"\(\S+?\)$");
+            string lessonType = reg.Match(rows[0]).Value;
+
+            rows[0] = rows[0].Substring(0, rows[0].Length - lessonType.Length);
+
+            return new Lesson()
+            {
+                Title = rows[0],
+                Type = lessonType.Replace("(", "").Replace(")", ""),
+                Subtitle = rows[1],
+                Room = room,
+                Number = lessonNumber,
+                Subgroup = subgroup == 0 ? null : subgroup
+            };
+        }
+        else
+        {
+            throw new NotImplementedException("Processing of single-line lessons is not implemented.");
+        }
+    }
+}
---
+++ E-Journal.Parser/PageParser.cs
@@ -0,0 +1,254 @@
+﻿using HtmlAgilityPack;
+using E_Journal.Parser.Models;
+using System.Globalization;
+
+namespace E_Journal.Parser;
+
+/// <summary>
+/// Contains methods for parsing schedule pages
+/// </summary>
+public static class PageParser
+{
+    /// <summary>
+    /// Parse weekly schedules from weekly page string
+    /// </summary>
+    public static IEnumerable<PreparsedTable> ParseWeeklySchedules(string pageText)
+    {
+        var document = BuildRootPageNode(pageText);
+        var contentNode = GetPageContentNode(document);
+
+        var nodeGroups = GetTitleAndTableGroupsFromPageContent(contentNode);
+        var preparsedTables = nodeGroups.Select(g => BuildWeeklyPreparsedTable(g.Item1, g.Item2));
+
+        return preparsedTables;
+    }
+
+    /// <summary>
+    /// Parse dayly schedules from dayly page string
+    /// </summary>
+    public static IEnumerable<PreparsedTable> ParseDaylySchedules(string pageText)
+    {
+        var document = BuildRootPageNode(pageText);
+        var contentNode = GetPageContentNode(document);
+
+        var tables = contentNode.Elements("table");
+
+        // take target rows from all dayly schedule tables
+        List<HtmlNode> tableRows = new();
+        tables.ToList()
+            .ForEach(t => tableRows.AddRange(GetTargetRowsOfDaylyTable(t)));
+
+        // split all dayly schedule tables rows into blocks of schedule
+        var tableBlocks = SplitDaylyTableRowsByTableBlocks(tableRows);
+
+        var preparsedTables = tableBlocks.Select(t => BuildDaylyPreparsedTable(t));
+
+        return preparsedTables;
+    }
+
+    /// <summary>
+    /// Build rootHtmlNode of html page
+    /// </summary>
+    /// <param name="textPage"></param>
+    /// <returns></returns>
+    private static HtmlNode BuildRootPageNode(string textPage)
+    {
+        HtmlDocument doc = new();
+        doc.LoadHtml(textPage);
+        return doc.DocumentNode;
+    }
+
+    /// <summary>
+    /// Get page content node from root page node
+    /// </summary>
+    private static HtmlNode GetPageContentNode(HtmlNode document)
+    {
+        return document.SelectSingleNode("//div[@class='col-sm-9 content']//div");
+    }
+
+    /// <summary>
+    /// Get target rows of the dayly schedule table
+    /// </summary>
+    private static IEnumerable<HtmlNode> GetTargetRowsOfDaylyTable(HtmlNode tableNode) =>
+        tableNode
+            .Element("tbody")
+            .Elements("tr")
+            .Where(tr =>
+                int.TryParse(tr.Element("td").InnerText, out _) ||
+                tr.Element("td").InnerText.Contains('№') ||
+                TryParseDaylyDateTableRow(tr, out _));
+
+    /// <summary>
+    /// Split single dayly schedule table by dates rows to blocks into dayly schedules
+    /// </summary>
+    /// <param name="tableRows">Rows of single dayly schedule table</param>
+    private static IEnumerable<HtmlNode[]> SplitDaylyTableRowsByTableBlocks(IEnumerable<HtmlNode> tableRows)
+    {
+        List<HtmlNode> groupsBlock = new();
+
+        foreach (var row in tableRows)
+        {
+            var result = TryParseDaylyDateTableRow(row, out _);
+
+            if (result)
+            {
+                if (groupsBlock.Count > 0)
+                    yield return groupsBlock.ToArray();
+
+                groupsBlock = new();
+            }
+
+            groupsBlock.Add(row);
+        }
+
+        yield return groupsBlock.ToArray();
+    }
+
+    /// <summary>
+    /// Try to get the date from the date row from the weekly schedule table 
+    /// </summary>
+    /// <returns>Returns true if the operation was successful</returns>
+    private static bool TryParseDaylyDateTableRow(HtmlNode tableRow, out DateTime result)
+    {
+        var node = tableRow
+            .Element("td")
+            ?.Element("p");
+
+        if (node == null ||
+            !node.InnerText.Contains("День - ") ||
+            !DateTime.TryParse(node.InnerText[7..], new CultureInfo("ru-Ru"), DateTimeStyles.None, out result))
+        {
+            result = default;
+            return false;
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Build preparsed table object based on rows of dayly schedule table
+    /// </summary>
+    private static PreparsedTable BuildDaylyPreparsedTable(HtmlNode[] tableRows)
+    {
+        DateTime tableDate;
+        TryParseDaylyDateTableRow(tableRows[0], out tableDate);
+
+        // take titles from titles row of dayly schedule table
+        var titles = tableRows[1]
+            .Elements("td")
+            .Skip(1)
+            .Select(r => r.InnerText.Trim())
+            .Where(t => !t.Equals("&nbsp;"))
+            .ToArray();
+
+        // create date array with date for each dayly schedule
+        var dates = Enumerable.Range(0, titles.Length)
+            .Select(i => tableDate)
+            .ToArray();
+
+        var targetRows = tableRows.Skip(2).ToArray();
+
+        return new PreparsedTable()
+        {
+            ColumnsTitles = titles,
+            ColumnsDates = dates,
+            Rows = targetRows
+        };
+    }
+
+    /// <summary>
+    /// Get enumerable of title and table groups from page content
+    /// </summary>
+    private static IEnumerable<(HtmlNode, HtmlNode?)> GetTitleAndTableGroupsFromPageContent(HtmlNode contentNode)
+    {
+        HtmlNode? title = null;
+        HtmlNode? table = null;
+
+        var scheduleTitleTag = GetTitleTagName(contentNode);
+        var targetElements = contentNode.ChildNodes
+            .Where(n => n.Name.Equals("table") ||
+            n.Name.Equals(scheduleTitleTag) && (n.InnerText.Contains("Группа - ") || n.InnerText.Contains("Преподаватель - ")));
+
+        foreach (HtmlNode node in targetElements)
+        {
+            if (node.Name.Equals(scheduleTitleTag))
+            {
+                if (title != null)
+                {
+                    yield return (title, table);
+
+                    table = null;
+                }
+
+                title = node;
+            }
+            else if (node.Name.Equals("table"))
+            {
+
+                table = node;
+            }
+        }
+
+        yield return (title, table);
+    }
+
+    /// <summary>
+    /// Get tag name of HtmlNode, that contains title of schedule table
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Throws if execution failed</exception>
+    private static string GetTitleTagName(HtmlNode contentNode) =>
+        contentNode
+        .ChildNodes
+        .FirstOrDefault(n => n.InnerText.Contains("Группа - ") || n.InnerText.Contains("Преподаватель - "))
+        ?.Name ?? throw new InvalidOperationException("Failed to determine schedule title tag.");
+
+    /// <summary>
+    /// Build preparsed table object based on title node and table node
+    /// </summary>
+    private static PreparsedTable BuildWeeklyPreparsedTable(HtmlNode title, HtmlNode? table)
+    {
+        // returns empty preparsed table object with single title, if schedule do not have table
+        if (table == null)
+        {
+            return new PreparsedTable()
+            {
+                ColumnsTitles = new[] { title.InnerText }
+            };
+        }
+
+        var tableRows = table
+            .Element("tbody")
+            .Elements("tr");
+
+        var columnsDates = ParseWeeklyScheduleDatesRow(tableRows.First());
+        var columnsTitles = Enumerable.Range(0, columnsDates.Length)
+            .Select(i => title.InnerText)
+            .ToArray();
+
+        return new PreparsedTable()
+        {
+            ColumnsTitles = columnsTitles,
+            ColumnsDates = columnsDates,
+            Rows = tableRows.Skip(2).ToArray()
+        };
+    }
+
+    /// <summary>
+    /// Parse table row with dates from weekly schedule and returns those dates
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Throws if no date is received</exception>
+    private static DateTime[] ParseWeeklyScheduleDatesRow(HtmlNode daysRow)
+    {
+        List<DateTime> days = new();
+
+        foreach (HtmlNode node in daysRow.Elements("td").Skip(1))
+        {
+            if (DateTime.TryParse(node.InnerText, new CultureInfo("ru-Ru"), DateTimeStyles.None, out DateTime buffer))
+            {
+                days.Add(buffer);
+            }
+        }
+
+        return days.Any() ? days.ToArray() : throw new InvalidOperationException("Не удалось определить даты учебных дней.");
+    }
+}
