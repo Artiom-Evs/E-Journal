34f0f1d22821edc1f2d59a21bb7fcb8b8f1359ba
Artiom-Evs <evs.a.s@mail.ru>
2022-10-16 23:25:06 +0300

    Add SchedulesController class

--- E-Journal.SchedulesApi/Program.cs
+++ E-Journal.SchedulesApi/Program.cs
@@ -2,13 +2,38 @@ namespace E_Journal.SchedulesApi;
 
 public static class Program
 {
+    public static void ConfigureServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public static void Configure(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+        
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+    }
+
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.ConfigureServices();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
-
+        app.Configure();
+        
+        app.MapControllers();
+        
         app.Run();
     }
 }
