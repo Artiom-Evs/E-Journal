52311e9adadf4a9e5f2554cb6222809b6c04a9dc
Artiom-Evs <evs.a.s@mail.ru>
2022-10-16 23:25:06 +0300

    Add SchedulesController class

---
+++ E-Journal.SchedulesApi/Controllers/SchedulesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace E_Journal.SchedulesApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SchedulesController : ControllerBase
+    {
+        [HttpGet("/{name}/", Name = nameof(Group))]
+        public IResult Group(string name)
+        {
+            return Results.Ok($"Requested group: {name}");
+        }
+    }
+}
