03d04bf05f25560cba36e40f5598d5e5c52628ee
Artiom-Evs <evs.a.s@mail.ru>
2022-10-21 19:27:54 +0300

    Add ApplicationDbContext class
    Postgres database container has also been added to the docker-compose comfiguration.

---
+++ E-Journal.SchedulesApi/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using E_Journal.Parser.Models;
+
+namespace E_Journal.SchedulesApi.Services;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+        Database.Migrate();
+    }
+
+    public DbSet<Lesson> Lessons { get; set; }
+}
