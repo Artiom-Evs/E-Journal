15970d8b07a56b7bfb8ae1ff2ce43f46aa9a978c
Artiom-Evs <evs.a.s@mail.ru>
2022-12-10 01:22:18 +0300

    Edit ApplicationDbContext class
    Primary key for the Lesson data models has been configured.
    Alternative key for the Group, Subject, Type, Teacher and Room data models has been configured.

--- E-Journal.SchedulesApi/Services/ApplicationDbContext.cs
+++ E-Journal.SchedulesApi/Services/ApplicationDbContext.cs
@@ -11,6 +11,16 @@ public class ApplicationDbContext : DbContext
         Database.Migrate();
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<SchedulesApi.Models.Lesson>().HasKey(m => new { m.GroupId, m.Date, m.Number, m.Subgroup });
+        modelBuilder.Entity<Group>().HasAlternateKey(m => m.Name);
+        modelBuilder.Entity<Subject>().HasAlternateKey(m => m.Name);
+        modelBuilder.Entity<Models.Type>().HasAlternateKey(m => m.Name);
+        modelBuilder.Entity<Teacher>().HasAlternateKey(m => m.Name);
+        modelBuilder.Entity<Room>().HasAlternateKey(m => m.Name);
+    }
+
     public DbSet<Lesson> Lessons { get; set; }
     public DbSet<Group> Groups { get; set; }
     public DbSet<Subject> Subjects { get; set; }
