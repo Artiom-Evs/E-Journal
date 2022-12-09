c501c0bdc3f73892a5fccb59432a4f03a6241b5b
Artiom-Evs <evs.a.s@mail.ru>
2022-12-10 01:04:10 +0300

    Add BaseRepository class
    This class is designed to store models inherited from abstract BaseModel class.
    BaseRepository class implements IBaseRepository interface.

---
+++ E-Journal.SchedulesApi/Services/BaseRepository.cs
@@ -0,0 +1,82 @@
+﻿using E_Journal.SchedulesApi.Models;
+
+namespace E_Journal.SchedulesApi.Services;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : BaseModel, new()
+{
+    protected readonly ApplicationDbContext _context;
+    
+    public BaseRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IQueryable<T> Items => _context.Set<T>().AsQueryable<T>();
+
+    public bool IsExists(int id)
+    {
+        return _context.Set<T>().Any(item => item.Id == id);
+    }
+    public bool IsExists(string name)
+    {
+        return _context.Set<T>().Any(item => item.Name == name);
+    }
+
+    public T? Get(int id)
+    {
+        return _context.Set<T>().FirstOrDefault(item => item.Id == id);
+    }
+    public T? Get(string name)
+    {
+        return _context.Set<T>().FirstOrDefault(item => item.Name == name);
+    }
+
+    public T GetOrCreate(string name)
+    {
+        var item = this.Get(name);
+
+        if (item == null)
+        {
+            item = _context.Set<T>().Add(new T() { Name = name }).Entity;
+            _context.SaveChanges();
+        }
+
+        return item;
+    }
+
+    public T? Create(string name)
+    {
+        if (this.IsExists(name))
+        {
+            return null;
+        }
+
+        var item = _context.Set<T>().Add(new T() { Name = name }).Entity;
+        _context.SaveChanges();
+        return item;
+    }
+    public T? Update(T item)
+    {
+        if (!this.IsExists(item.Id))
+        {
+            return null;
+        }
+
+        item = _context.Set<T>().Update(item).Entity;
+        _context.SaveChanges();
+        return item;
+    }
+    public T? Delete(int id)
+    {
+        var item = this.Get(id);
+
+        if (item == null)
+        {
+            return null;
+        }
+
+        _context.Set<T>().Remove(item);
+        _context.SaveChanges();
+        return item;
+    }
+}
---
+++ E-Journal.SchedulesApi/Services/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using E_Journal.SchedulesApi.Models;
+
+namespace E_Journal.SchedulesApi.Services;
+
+public interface IBaseRepository<T> where T : class, IBaseModel, new()
+{
+    IQueryable<T> Items { get; }
+
+    bool IsExists(int id);
+    bool IsExists(string name);
+
+    T? Get(int id);
+    T? Get(string name);
+    T GetOrCreate(string name);
+    T? Create(string name);
+    T? Update(T item);
+    T? Delete(int id);
+}
