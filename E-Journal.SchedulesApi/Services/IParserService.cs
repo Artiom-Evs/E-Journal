e0df22f0c8db4c4e9db8d7c5df20d42319c1a667
Artiom-Evs <evs.a.s@mail.ru>
2022-12-10 02:01:46 +0300

    Add ParserService class
    This class is designed to parse schedules webpage.

---
+++ E-Journal.SchedulesApi/Services/IParserService.cs
@@ -0,0 +1,8 @@
+﻿using E_Journal.Parser.Models;
+
+namespace E_Journal.SchedulesApi.Services;
+
+public interface IParserService
+{
+    IEnumerable<Lesson> ParseDaylySchedulesPage(string pageText);
+}
---
+++ E-Journal.SchedulesApi/Services/ParserService.cs
@@ -0,0 +1,79 @@
+﻿using E_Journal.Parser;
+using E_Journal.Parser.Models;
+using E_Journal.SchedulesApi.Extensions;
+using System.Runtime.CompilerServices;
+
+namespace E_Journal.SchedulesApi.Services;
+
+public class ParserService : IParserService
+{
+    private readonly ILogger<ParserService> _logger;
+
+    public ParserService(ILogger<ParserService> logger)
+    {
+        _logger = logger;
+    }
+
+    public IEnumerable<Lesson> ParseDaylySchedulesPage(string pageText)
+    {
+        IEnumerable<PreparsedTable> preparsedTables;
+
+        try
+        {
+            preparsedTables = PageParser.ParseDaylySchedules(pageText);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ">> Exception occurred while schedule page parsing\r\n");
+            yield break;
+        }
+
+        foreach (var preparsedTable in preparsedTables)
+        {
+            IEnumerable<IEnumerable<PreparsedCell>> preparsedCells;
+
+            try
+            {
+                preparsedCells = TableParser.ParseTable(preparsedTable);
+            }
+            catch (Exception ex)
+            {
+                string message = ">> Exception occurred while schedule table parsing\r\n" +
+                    preparsedTable.ToErrorString();
+
+                _logger.LogError(ex, message);
+                continue;
+            }
+
+            foreach (var (dayCells, groupName, date) in preparsedCells.Zip(preparsedTable.ColumnsTitles, preparsedTable.ColumnsDates))
+            {
+                foreach (var cell in dayCells)
+                {
+                    List<Lesson> lessons = new();
+                    try
+                    {
+                        lessons.AddRange(CellParser.ParseCell(cell));
+                    }
+                    catch (Exception ex)
+                    {
+                        string message = ">> Exception occurred while schedule cell parsing\r\n" +
+                                cell.ToErrorString();
+
+                        _logger.LogError(ex, message);
+                    }
+
+                    foreach (var lesson in lessons)
+                    {
+                        yield return lesson;
+                    }
+                }
+            }
+        }
+    }
+
+    private static bool CompareLessons(IEnumerable<Lesson> sequence1, IEnumerable<Lesson> sequence2)
+    {
+        HashSet<Lesson> lessonsSet = new(sequence1);
+        return lessonsSet.SetEquals(sequence2);
+    }
+}
