e6d280616869ebf1d69b56874dc6982a3f6a113f
Artiom-Evs <evs.a.s@mail.ru>
2022-12-13 13:03:26 +0300

    Add lesson comparison classes
    LessonEqualityComparer class has been added. It is designed to compare lessons across all properties.
    LessonPrimaryKeyEqualityComparer class has been added. It is designed to compare lessons across primary key properties.

---
+++ E-Journal.SchedulesApi/Infrastructure/LessonEqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using E_Journal.SchedulesApi.Models;
+
+namespace E_Journal.SchedulesApi.Infrastructure;
+
+public class LessonEqualityComparer : IEqualityComparer<Lesson>
+{
+    public bool Equals(Lesson? x, Lesson? y)
+    {
+        if (x == y) return true;
+
+        if (x == null || y == null) return false;
+
+        bool result =
+            x.Group.Name == y.Group.Name
+            && x.Subject.Name == y.Subject.Name
+            && x.Type.Name == y.Type.Name
+            && x.Teacher.Name == y.Teacher.Name
+            && x.Room.Name == y.Room.Name
+            && x.Subgroup == y.Subgroup
+            && x.Number == y.Number
+            && x.Date == y.Date;
+
+        return result;
+    }
+
+    public int GetHashCode([DisallowNull] Lesson obj)
+    {
+        return obj.ToString().GetHashCode();
+    }
+}
---
+++ E-Journal.SchedulesApi/Infrastructure/LessonPrimaryKeyEqualityComparer.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using E_Journal.SchedulesApi.Models;
+
+namespace E_Journal.SchedulesApi.Infrastructure;
+
+public class LessonPrimaryKeyEqualityComparer : IEqualityComparer<Lesson>
+{
+    public bool Equals(Lesson? x, Lesson? y)
+    {
+        if (x == y) return true;
+
+        if (x == null || y == null) return false;
+
+        bool result =
+            x.Group.Name == y.Group.Name
+            && x.Date == y.Date
+            && x.Subgroup == y.Subgroup
+            && x.Number == y.Number;
+
+        return result;
+    }
+
+    public int GetHashCode([DisallowNull] Lesson obj)
+    {
+        string str = 
+            obj.Group.Name +
+            obj.Date.ToString() +
+            obj.Number.ToString() +  
+            obj.Subgroup.ToString();
+
+        return str.GetHashCode();
+    }
+}
