a8a1c5463218ed59ee6b8219d5ec96bdbb0a1821
Artiom-Evs <evs.a.s@mail.ru>
2022-12-13 14:58:05 +0300

    Edit SchedulesController class
    Output JSON format has been changed in the SchedulesController class. Now It does not return any identificators in the JSON response.
    OutputLessonModel class has been added.

--- E-Journal.SchedulesApi/Controllers/SchedulesController.cs
+++ E-Journal.SchedulesApi/Controllers/SchedulesController.cs
@@ -4,154 +4,164 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace E_Journal.SchedulesApi.Controllers
+namespace E_Journal.SchedulesApi.Controllers;
+
+[ApiController]
+public class SchedulesController : ControllerBase
 {
-    [ApiController]
-    public class SchedulesController : ControllerBase
+    private readonly ILessonsRepository _repository;
+    private readonly IBaseRepository<Group> _groups;
+    private readonly IBaseRepository<Teacher> _teachers;
+
+    public SchedulesController(ILessonsRepository repository, IBaseRepository<Group> groups, IBaseRepository<Teacher> teachers)
     {
-        private readonly ILessonsRepository _repository;
-        private readonly IBaseRepository<Group> _groups;
-        private readonly IBaseRepository<Teacher> _teachers;
+        _repository = repository;
+        _groups = groups;
+        _teachers = teachers;
+    }
 
-        public SchedulesController(ILessonsRepository repository, IBaseRepository<Group> groups, IBaseRepository<Teacher> teachers)
+    [HttpGet("api/[controller]/groups/{name}/", Name = nameof(GetGroup))]
+    [Consumes("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetGroup(string name, [FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+    {
+        var group = _groups.Get(name);
+
+        if (group == null)
         {
-            _repository = repository;
-            _groups = groups;
-            _teachers = teachers;
+            return NotFound();
         }
 
-        [HttpGet("api/[controller]/groups/{name}/", Name = nameof(GetGroup))]
-        [Consumes("application/json")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult GetGroup(string name, [FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+        var query = _repository.Lessons
+            .Where(l => l.GroupId == group.Id);
+
+        if (!query.Any())
         {
-            var group = _groups.Get(name);
-
-            if (group == null)
-            {
-                return NotFound();
-            }
-
-            var query = _repository.Lessons
-                .Where(l => l.GroupId == group.Id);
-
-            if (!query.Any())
-            {
-                return NotFound();
-            }
-
-            if (startDate != default)
-            {
-                query = query.Where(l => l.Date >= startDate);
-            }
-
-            if (endDate != default)
-            {
-                query = query.Where(l => l.Date <= endDate);
-            }
-
-            return new JsonResult(query);
+            return NotFound();
         }
 
-        [HttpGet("api/[controller]/groups/all/", Name = nameof(GetAllGroups))]
-        [Consumes("application/json")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult GetAllGroups([FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+        if (startDate != default)
         {
-            var query = _repository.Lessons;
-
-            if (startDate != default)
-            {
-                query = query.Where(l => l.Date >= startDate);
-            }
-
-            if (endDate != default)
-            {
-                query = query.Where(l => l.Date <= endDate);
-            }
-
-            var result = query.AsEnumerable()
-                .GroupBy(l => l.Group.Name)
-                .ToDictionary(g => g.Key, g => g.ToArray());
-
-            return new JsonResult(result);
+            query = query.Where(l => l.Date >= startDate);
         }
 
-        [HttpGet("api/[controller]/teathers/{name}/", Name = nameof(GetTeather))]
-        [Consumes("application/json")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult GetTeather(string name, [FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+        if (endDate != default)
         {
-            var teacher = _teachers.Get(name);
-
-            if (teacher == null)
-            {
-                return NotFound();
-            }
-
-            var query = _repository.Lessons
-                .Where(l => l.TeacherId == teacher.Id);
-
-            if (!query.Any())
-            {
-                return NotFound();
-            }
-
-            if (startDate != default)
-            {
-                query = query.Where(l => l.Date >= startDate);
-            }
-
-            if (endDate != default)
-            {
-                query = query.Where(l => l.Date <= endDate);
-            }
-
-            return new JsonResult(query);
+            query = query.Where(l => l.Date <= endDate);
         }
 
-        [HttpGet("api/[controller]/teathers/all/", Name = nameof(GetAllTeathers))]
-        [Consumes("application/json")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult GetAllTeathers([FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+        var result = query.AsEnumerable()
+            .Select(l => new OutputLessonModel(l));
+
+        return new JsonResult(result);
+    }
+
+    [HttpGet("api/[controller]/groups/all/", Name = nameof(GetAllGroups))]
+    [Consumes("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetAllGroups([FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+    {
+        var query = _repository.Lessons;
+
+        if (startDate != default)
         {
-            var query = _repository.Lessons;
-
-            if (startDate != default)
-            {
-                query = query.Where(l => l.Date >= startDate);
-            }
-
-            if (endDate != default)
-            {
-                query = query.Where(l => l.Date <= endDate);
-            }
-
-            var result = query.AsEnumerable()
-                .GroupBy(l => l.Teacher.Name)
-                .ToDictionary(g => g.Key, g => g.ToArray());
-
-            return new JsonResult(result);
+            query = query.Where(l => l.Date >= startDate);
         }
 
-        [HttpGet("api/[controller]/all/", Name = nameof(GetAll))]
-        [Consumes("application/json")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult GetAll([FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+        if (endDate != default)
         {
-            var query = _repository.Lessons;
-
-            if (startDate != default)
-            {
-                query = query.Where(l => l.Date >= startDate);
-            }
-
-            if (endDate != default)
-            {
-                query = query.Where(l => l.Date <= endDate);
-            }
-            
-            return new JsonResult(query);
+            query = query.Where(l => l.Date <= endDate);
         }
+
+        var result = query.AsEnumerable()
+            .Select(l => new OutputLessonModel(l))
+            .GroupBy(l => l.Group)
+            .ToDictionary(g => g.Key, g => g.ToArray());
+
+        return new JsonResult(result);
+    }
+
+    [HttpGet("api/[controller]/teathers/{name}/", Name = nameof(GetTeather))]
+    [Consumes("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetTeather(string name, [FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+    {
+        var teacher = _teachers.Get(name);
+
+        if (teacher == null)
+        {
+            return NotFound();
+        }
+
+        var query = _repository.Lessons
+            .Where(l => l.TeacherId == teacher.Id);
+
+        if (!query.Any())
+        {
+            return NotFound();
+        }
+
+        if (startDate != default)
+        {
+            query = query.Where(l => l.Date >= startDate);
+        }
+
+        if (endDate != default)
+        {
+            query = query.Where(l => l.Date <= endDate);
+        }
+
+        var result = query.AsEnumerable()
+            .Select(l => new OutputLessonModel(l));
+
+        return new JsonResult(result);
+    }
+
+    [HttpGet("api/[controller]/teathers/all/", Name = nameof(GetAllTeathers))]
+    [Consumes("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetAllTeathers([FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+    {
+        var query = _repository.Lessons;
+
+        if (startDate != default)
+        {
+            query = query.Where(l => l.Date >= startDate);
+        }
+
+        if (endDate != default)
+        {
+            query = query.Where(l => l.Date <= endDate);
+        }
+
+        var result = query.AsEnumerable()
+            .Select(l => new OutputLessonModel(l))
+            .GroupBy(l => l.Teacher)
+            .ToDictionary(g => g.Key, g => g.ToArray());
+
+        return new JsonResult(result);
+    }
+
+    [HttpGet("api/[controller]/all/", Name = nameof(GetAll))]
+    [Consumes("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetAll([FromQuery] DateTime startDate = default, [FromQuery] DateTime endDate = default)
+    {
+        var query = _repository.Lessons;
+
+        if (startDate != default)
+        {
+            query = query.Where(l => l.Date >= startDate);
+        }
+
+        if (endDate != default)
+        {
+            query = query.Where(l => l.Date <= endDate);
+        }
+
+        var result = query.AsEnumerable()
+            .Select(l => new OutputLessonModel(l));
+
+        return new JsonResult(result);
     }
 }
---
+++ E-Journal.SchedulesApi/Models/OutputLessonModel.cs
@@ -0,0 +1,25 @@
+﻿namespace E_Journal.SchedulesApi.Models;
+
+public class OutputLessonModel
+{
+    public OutputLessonModel(Lesson lesson)
+    {
+        this.Group = lesson.Group.Name;
+        this.Subject = lesson.Subject.Name;
+        this.Type = lesson.Type.Name;
+        this.Teacher = lesson.Teacher.Name;
+        this.Room = lesson.Room.Name;
+        this.Date = lesson.Date;
+        this.Number = lesson.Number;
+        this.Subgroup = lesson.Subgroup;
+    }
+
+    public string Group { get; set; }
+    public string Subject { get; set; }
+    public string Type { get; set; }
+    public string Teacher { get; set; }
+    public string Room { get; set; }
+    public DateTime Date { get; set; }
+    public int Number { get; set; }
+    public int Subgroup { get; set; }
+}
