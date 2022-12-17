cd60388292b4b00afade1d96d9a858723da356b8
Artiom-Evs <evs.a.s@mail.ru>
2022-12-17 15:08:40 +0300

    Add Student data model
    This data model represents one student in the journal.

---
+++ E-Journal.JournalApi/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using E_Journal.JournalApi.Models;
+
+namespace E_Journal.JournalApi.Models;
+
+public record Student
+{
+    public Student()
+    {
+    }
+
+    public Student(string name, Group group)
+    {
+        Name = name;
+        Group = group;
+    }
+
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public Group Group { get; init; }
+    public int GroupId { get; init; }
+
+    public override string ToString()
+    {
+        return
+            $"{nameof(this.Name)}: {this.Name}\r\n" +
+            $"{nameof(this.Group)}: {this.Group?.Name ?? "NULL"}\r\n";
+    }
+}
