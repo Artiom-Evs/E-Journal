facac9b8ea1ac226b06dafba98ed4a6db7727e18
Artiom-Evs <evs.a.s@mail.ru>
2023-01-15 17:30:58 +0300

    Add Identity project
    This project is an external identity server. It will contain authentication and authorization features for public applications.

---
+++ E-Journal.Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using E_Journal.Identity.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace E_Journal.Identity.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
