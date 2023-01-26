f5acceebb59a7022b0e1c9e32ccd670362d3f3e8
Artiom-Evs <evs.a.s@mail.ru>
2023-01-26 22:33:47 +0300

    Delete unnecessary pages

--- e-journal.webspa/src/AppRoutes.js
+++ e-journal.webspa/src/AppRoutes.js
@@ -1,6 +1,4 @@
 import ApiAuthorzationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
 import { WeeklySchedule } from './components/WeeklySchedule';
 import { Home } from "./components/Home";
 
@@ -8,15 +6,6 @@ const AppRoutes = [
   {
     index: true,
     element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    requireAuth: true,
-    element: <FetchData />
   },
   {
     path: '/groups-schedule',
